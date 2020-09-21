c28d63cf8d83376e0c119b7d50dd38a5a1598fe7
wilfred <wilfredluijk@gmail.com>
2020-09-21 20:18:59 +0200

    Add extra figures, and improve movement

--- script/main.js
+++ script/main.js
@@ -2,23 +2,13 @@ let gameTick = 0;
 let stagingType = 0;
 let activeBlock = {
     type: -1,
-    coords: [{x: 0, y: 0}], orientation: 1
+    coords: [[{x: 0, y: 0}]], orientation: 1
 };
 
 
 // [[{filled: 1, div: div}][]]
 const gameScreen = [];
 let pause = false;
-const blockFigures = [{
-    name: "long",
-    type: 1,
-    blocks: [
-        [1],
-        [1],
-        [1],
-        [1],
-    ],
-}]
 
 main();
 
@@ -31,7 +21,7 @@ function main() {
     document.onkeydown = handleArrowKeys;
 
 
-    setInterval(gameLoop, 400)
+    setInterval(gameLoop, 1000)
 }
 
 function createDivWithClass(className) {
@@ -61,33 +51,44 @@ function moveActive(horizontalStep) {
     let canMove = true;
     console.log('moveSideWays', activeBlock)
     for (let i = activeBlock.coords.length - 1; i >= 0; i--) {
-
-        const coord = activeBlock.coords[i];
-        const newX = coord.x + horizontalStep;
-
-        if (coord.y >= 0) {
-            const row = gameScreen[coord.y];
-            const block = row[newX];
-            if (block.filled === 1) {
+        const row = activeBlock.coords[i];
+        let block = horizontalStep === 1 ? row[row.length - 1] : row[0];
+        const newX = block.x + horizontalStep;
+        console.log('new x', newX);
+        if (newX > 9 || newX < 0) {
+            canMove = false;
+            break;
+        }
+        if (block.y >= 0) {
+            console.log(block)
+            const row = gameScreen[block.y];
+            const blockElement = row[newX];
+            if (blockElement.filled === 1) {
                 canMove = false;
                 break;
             }
         }
-
     }
+
     if (canMove && activeBlock.type !== -1) {
         console.log('cam move sideways', activeBlock)
         for (let i = activeBlock.coords.length - 1; i >= 0; i--) {
-            const coord = activeBlock.coords[i];
-            if (coord.y >= 0) {
-                gameScreen[coord.y][coord.x].filled = 0;
-                coord.x += horizontalStep;
-                gameScreen[coord.y][coord.x].filled = 1;
-            } else {
-                coord.x += horizontalStep;
+            const slice = activeBlock.coords[i].slice();
+
+            if (horizontalStep > 0) {
+                slice.reverse();
             }
 
-
+            slice.forEach(block => {
+                if (block.y >= 0) {
+                    gameScreen[block.y][block.x].filled = 0;
+                    block.x += horizontalStep;
+                    gameScreen[block.y][block.x].filled = 1;
+                    console.log(block.y, block.x);
+                } else {
+                    block.x += horizontalStep;
+                }
+            })
         }
     }
 }
@@ -113,14 +114,6 @@ function handleArrowKeys(e) {
 
 }
 
-function getStartCoordsForType(stagingType) {
-    switch (stagingType) {
-        case 1:
-            return [{x: 5, y: -4}, {x: 5, y: -3}, {x: 5, y: -2}, {x: 5, y: -1}];
-    }
-    return [];
-}
-
 function initiateNewGameBlock() {
     activeBlock.type = stagingType;
     activeBlock.coords = getStartCoordsForType(stagingType)
@@ -132,41 +125,51 @@ function resetActiveBlock() {
 }
 
 function handleVerticalMovement() {
-    for (let i = activeBlock.coords.length - 1; i >= 0; i--) {
-        const coord = activeBlock.coords[i];
-        const newY = coord.y + 1;
+    let invalidMove = false;
+    const figureRowCount = activeBlock.coords.length - 1;
+    for (let i = figureRowCount; i >= 0; i--) {
+        const figureRow = activeBlock.coords[i];
 
-        if (newY >= 0) {
-            if (newY >= gameScreen.length) {
-                console.log('end of game board');
-                // end of game board
-                resetActiveBlock();
-                continue;
-            }
+        figureRow.forEach((position, index) => {
+            const newY = position.y + 1;
+            if (newY >= 0 && !invalidMove) {
+                invalidMove = newY >= gameScreen.length;
 
-            const row = gameScreen[newY];
-            const blockElement = row[coord.x];
-
-            console.log('gonna take a look at block element', blockElement, newY, coord.x);
-            if (blockElement.filled === 1) {
-                // hitting other block, full stop
-                // TODO: Handle lose case
-                if (i === activeBlock.coords.length - 1) {
-                    console.log('hitting other block, reset');
+                if (!invalidMove) {
+                    if (i === figureRowCount) {
+                        invalidMove = gameScreen[newY][position.x].filled === 1;
+                    } else {
+                        const blockBelow = activeBlock.coords[i + 1]
+                            .some(blockCoordinates => blockCoordinates.x === position.x
+                                && blockCoordinates.y === newY);
+                        if (!blockBelow) {
+                            invalidMove = gameScreen[newY][position.x].filled === 1;
+                        }
+                    }
+                }
+                if (invalidMove) {
+                    console.log('Hitting end of board, or other block');
                     resetActiveBlock();
                 }
-            } else {
-                blockElement.filled = 1;
-
-                if (coord.y >= 0) {
-                    const oldRow = gameScreen[coord.y]
-                    const oldBlockElement = oldRow[coord.x]
-                    console.log('clear old position', coord)
-                    oldBlockElement.filled = 0;
-                }
             }
+        });
+    }
+
+    if (!invalidMove) {
+        for (let i = figureRowCount; i >= 0; i--) {
+            const figureRow = activeBlock.coords[i];
+            figureRow.forEach((position, index) => {
+                const newY = position.y + 1;
+
+                if (position.y >= 0) {
+                    gameScreen[position.y][position.x].filled = 0
+                }
+                if (newY >= 0) {
+                    gameScreen[newY][position.x].filled = 1;
+                }
+                position.y = newY;
+            });
         }
-        coord.y = newY;
     }
 }
 
@@ -186,17 +189,13 @@ function renderScreen() {
 function gameLoop() {
     if (!pause) {
         if (stagingType === 0) {
-            //TODO: Fetch logic for new type
-            stagingType = 1;
+            stagingType = Math.floor(Math.random() * 7) + 1;
         }
 
         if (activeBlock.type === -1) {
             initiateNewGameBlock();
             stagingType = 0;
         }
-
-        const blockFigure = blockFigures.find(block => block.type === activeBlock.type);
-
 
         handleVerticalMovement();
 
@@ -205,4 +204,52 @@ function gameLoop() {
 
         gameTick++;
     }
+}
+
+function getStartCoordsForType(stagingType) {
+    let returnValue = [];
+    switch (stagingType) {
+        case 1:
+            // #
+            // #
+            // #
+            // #
+            returnValue = [[{x: 5, y: -4}], [{x: 5, y: -3}], [{x: 5, y: -2}], [{x: 5, y: -1}]];
+            break
+        case 2:
+            // #
+            // #
+            // ##
+            returnValue = [[{x: 5, y: -3}], [{x: 5, y: -2}], [{x: 5, y: -1}, {x: 6, y: -1}]];
+            break;
+        case 3:
+            //  #
+            //  #
+            // ##
+            returnValue = [[{x: 5, y: -3}], [{x: 5, y: -2}], [{x: 4, y: -1}, {x: 5, y: -1}]]
+            break;
+        case 4:
+            // ##
+            // ##
+            returnValue = [[{x: 5, y: -2}, {x: 6, y: -2}], [{x: 5, y: -1}, {x: 6, y: -1}]]
+            break;
+        case 5:
+            // ##
+            //  ##
+            returnValue = [[{x: 5, y: -2}, {x: 6, y: -2}], [{x: 6, y: -1}, {x: 7, y: -1}]]
+            break;
+        case 6:
+            //  ##
+            // ##
+            returnValue = [[{x: 6, y: -2}, {x: 7, y: -2}], [{x: 5, y: -1}, {x: 6, y: -1}]]
+            break;
+        case 7:
+            // #
+            // ##
+            // #
+            returnValue = [[{x: 5, y: -3}], [{x: 5, y: -2}, {x: 6, y: -2}], [{x: 6, y: -1}]];
+            break;
+    }
+    console.log('new type', stagingType);
+    return returnValue;
 }
